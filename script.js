a5755d3e35ed46c1f1b814a17950e871500ca881
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2020-03-08 19:50:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-$('.contact-element-telephone').hover(function(){$('.contact-link-telephone').toggleClass('hover-white');});
-$('.contact-element-mail').hover(function(){$('.contact-link-mail').toggleClass('hover-white');});
+$('.contact-element-telephone').hover(function(){$('.contact-link-telephone').toggleClass('hover-white');}); //setting white color - telephone button
+$('.contact-element-mail').hover(function(){$('.contact-link-mail').toggleClass('hover-white');}); //setting white color - mail button
